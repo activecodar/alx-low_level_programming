c73bc017b3851622e96e4317204ef4940f575a00
activecodar <activecodar@gmail.com>
2022-09-08 08:19:08 -0700

    puts text
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - program puts out a text
+*
+* Return: 0 terminator
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
