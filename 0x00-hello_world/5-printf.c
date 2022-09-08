e7d279c411b36b07b64b1394e8e93fa9f097fe6d
activecodar <activecodar@gmail.com>
2022-09-08 08:43:13 -0700

    print text with printf
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - program printsf out a text
+*
+* Return: 0 terminator
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
