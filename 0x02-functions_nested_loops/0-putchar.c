95dca9ed578e2a6186e95befcbd488cac49401a1
activecodar <activecodar@gmail.com>
2022-09-12 08:21:42 -0700

    print _putchar
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - outputs _putchar.
+ *
+ * Return: 0 as the terminator
+ */
+
+int main(void)
+{
+	char str[8] = "_putchar";
+	int counter;
+
+	for (counter = 1; counter < 8; counter++)
+	{
+		_putchar(str[counter]);
+	};
+	_putchar('\n');
+
+	return (0);
+}
