cbf3db2740e0cc8954a2812e85a437a50f7dfb58
activecodar <activecodar@gmail.com>
2022-10-11 08:33:14 -0700

    dog struct definition
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - provides details about dog
+ * @name: name of the dog
+ * @age: age of dog
+ * @owner: name of dog owner
+ *
+ * Description: attributes of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
