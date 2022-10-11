2b7fc2101be413c19cb9afc66c1b71df7a6f7029
activecodar <activecodar@gmail.com>
2022-10-11 09:45:15 -0700

    maloc free
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - main entry
+ * @ac: int input
+ * @av: double pointer array
+ * Return: 0
+ */
+
+char *argstostr(int ac, char **av)
+{
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	char *new_str;
+
+	for (idx = 0;)
+}
