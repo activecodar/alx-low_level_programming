dc2139d7f3ff6b20203351d834e3c326281beadd
activecodar <activecodar@gmail.com>
2022-10-26 00:43:51 -0700

    pop element from list
    Signed-off-by: activecodar <activecodar@gmail.com>

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Pops the head node of a listint_t list.
+ * @head: A pointer to head of the listint_t list.
+ *
+ * Return: Head's data else 0
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp_head;
+	int return_data;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		temp_head = *head;
+		return_data = (*head)->n;
+		*head = (*head)->next;
+		free(temp_head);
+		return (return_data);
+	}
+}
